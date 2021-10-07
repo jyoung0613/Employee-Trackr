2c7b2867d4729412b9b4ca797f077de4bd302cf2
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-10-07 13:15:26 -0600

    added the sql code to create the database and tables its all working

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+        ON DELETE CASCADE
+);
