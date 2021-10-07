536235456d6e9e6f76449f6a77d4934111379210
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-10-07 12:26:01 -0600

    added additional items to index file and connections file relating to npm packages and dependancies

---
+++ index.js
@@ -0,0 +1,31 @@
+// Get the connection query to run asynchronously after receiving the information it needs from the inquirer package and funcitons.
+// Validate employees, roles, and departments against existing
+// Validate salary as a number
+
+// NPM Packages
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const colors = require('colors');
+
+// Dependencies
+const connection = require('./config/connections');
+const { connect } = require('./config/connections');
+
+
+// Arrays
+
+// Colorized fonts
+
+// Welcome and Instructions
+
+// CRUD functions
+// CREATE (Add)
+
+// READ (View)
+
+
+// UPDATE (Change)
+
+
+// Delete (Remove)
